929cdcb010f49eae51160dbf4e65b1616f30cfd9
Harry Park <phg98@naver.com>
2019-12-28 16:53:43 +0900

    Fix timezone of log

---
+++ TodayILearned.md
@@ -0,0 +1,7 @@
+# Today I Learned ...
+
+## 2019-12-28 
+* 로그출력시 타임존에 맞게 출력되도록 수정
+    * 참고 : https://medium.com/front-end-weekly/node-js-logs-in-local-timezone-on-morgan-and-winston-9e98b2b9ca45
+* moment 설치후에 mongodb-memory-server 모듈을 못찾는 문제가 있어서 다시 설치함.
+    * npm i mongodb-memory-server --save-dev
--- configs/winston.js
+++ configs/winston.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var winston = require('winston')
+const moment = require('moment-timezone')
 
 const logDir = __dirname + '/../logs'
 
@@ -7,32 +8,73 @@ if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir)
 }
 
+
+const myFormat = winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.label} - ${info.message}`);
+// function myTimeStamp () {
+//     // return new Date().toString(); 
+//     return moment().format('hh:mm:ss.SSS'); 
+// };
+const appendTimestamp = winston.format((info, opts) => {
+    if(opts.tz)
+        info.timestamp = moment().tz(opts.tz).format();
+    return info;
+  });
+
 const infoTransport = new winston.transports.File({
     filename: 'info.log',
     dirname: logDir,
-    level: 'info'
+    level: 'info',
+    json: false,
+    timestamp: true,
+    colorize: true,
+    format: winston.format.combine(
+        winston.format.label({ label: 'heartbeat' }),
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        myFormat
+    )
 })
 
 const errorTransport = new winston.transports.File({
     filename: 'error.log',
     dirname: logDir,
-    level: 'error'
+    level: 'error',
+    json: false,
+    timestamp: true,
+    colorize: true,
+    format: winston.format.combine(
+        winston.format.label({ label: 'heartbeat' }),
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        myFormat
+    )
 })
 
 const consoleTransport = new (winston.transports.Console)({
+
     level: 'debug',
     handleExceptions: true,
     json: false, // 로그형태를 json으로도 뽑을 수 있다.
     colorize: true,
-    // format: combine(
-    //     format.colorize(),
-    //     format.printf(
-    //       info => `${info.timestamp} ${info.level}: ${info.message}`
-    //     )
-    //   )
+    timestamp: true,
+    format: winston.format.combine(
+        winston.format.label({ label: 'heartbeat' }),
+        appendTimestamp({ tz: 'Asia/Seoul' }),
+        winston.format.colorize(),
+        myFormat
+    )
   })
 
+
 const logger = winston.createLogger({
+    // timestamp: myTimeStamp,
+    // json: false,
+    // format: winston.format.combine(
+    //     winston.format.label({ label: 'main' }),
+    //     winston.format.colorize(),
+    //     winston.format.timestamp(),
+    //   myFormat
+    // ),
     transports: [infoTransport, errorTransport, consoleTransport]
 })
 
