bc5d270b529097877db5b8747b69bae7198711a6
Harry Park <phg98@naver.com>
2020-01-07 23:28:47 +0900

    Add test for ping API

--- test/ping.test.js
+++ test/ping.test.js
@@ -1,5 +1,7 @@
 var request = require("supertest")
-var chai_expect = require("chai").expect
+const chai = require("chai")
+var chai_expect = chai.expect
+// chai.use(require('chai-as-promised'))
 var app;
 
 before(async () => {
@@ -14,42 +16,37 @@ before(async () => {
 describe("ping", function () {
     
     var data = {
-        serverId: "5sec",
-        serverName: "5sec",
-        timeout: 5000,
+        serverId: "1sec",
+        serverName: "1sec",
+        timeout: 1000,
         phoneNumber: "+12345678",
         isTemp:true
     };
-
+    
     it("should err when no server", function (done) {
         request(app).get("/ping/server_not_exists")
             .expect(404)
             .end(done);
     })
+
     
-    // it("should show user", function (done) {
-    //     request(app).get("/users")
-    //         .expect(200)
-    //         .expect((res)=> {console.log(res.body);chai_expect(res.body[0]).to.be.an('Object').that.includes({serverId: "5sec"})})
-    //         .end(done)
-    // })
-
-    // it("should delete user", function (done) {
-    //     // Arrange
-    //     // 이미 저장된 데이터가 1개 있으니 이것을 지우자.
-
-    //     // Act
-    //     request(app).delete("/users/"+data.serverId)
-    //         .expect(200)
-    //         .end(() => {
-
-    //             // Assert        
-    //             request(app).get("/users")
-    //                 .expect(200)
-    //                 .expect((res)=> {
-    //                     chai_expect(res.body).to.be.empty;
-    //                 })
-    //                 .end(done)
-    //         })
-    // })
+    it("should activate when first ping", function (done) {
+        // Arrange
+        request(app).post("/users/")
+        .send(data)
+        .expect(200)
+        .end(() => {
+            // Act
+            request(app).get("/ping/" + data.serverId)
+                .expect(200)
+                .end(()=>{
+                    // Assert                    
+                    request(app).get("/users/" + data.serverId)
+                    .expect(200)
+                    .expect((res)=> {chai_expect(res.body[0]).to.be.an('Object').that.includes({currentStatus: "Up"})})
+                    .end(done)
+                })
+        })
+    })
+ 
 })
